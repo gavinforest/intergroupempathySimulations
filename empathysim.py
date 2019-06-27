26e879715a4917dc629b67f732a36b1996818042
Gavin Rees <gavinrees@college.harvard.edu>
2019-06-27 13:30:06 -0400

    Completed initial simulation code, no monitoring or statistics collection. Untested.

--- empathysim.py
+++ empathysim.py
@@ -5,11 +5,19 @@ NUMAGENTS = 100
 
 NUMGENERATIONS = 1500
 
+NORM = np.array([[]])
+
 #observation error
 Eobs = 0.0
 
 #cooperation error
 Ecoop = 0.0
+
+#imitation pressure
+w = 1.0
+
+#random strategy adoption
+u = 0.0025
 
 class DonationGame:
 	def __init__(self, benefit, cost):
@@ -17,23 +25,20 @@ class DonationGame:
 		self.c = cost
 		self.payoffMatrix = np.array([[0, benefit],[-cost, benefit - cost]])
 
+	def moveError(self, strat):
+		if np.random.random() < Ecoop and strat:
+			return 0
+		else:
+			return strat
+
 	def play(self, strat1, strat2):
-		if strat1 == 1:
-			if np.random.random() < Ecoop:
-				strat1 == 0
-
-		if strat2 == 1:
-			if np.random.random() < Ecoop:
-				strat2 = 0
-
-		
 		return self.payoffMatrix[strat1,strat2], self.payoffMatrix[strat2, strat1]
 
 class Agent:
-	def __init__(self, AgentType, empathyIn = 0.0, empathyOut = 0.0, strat = None):
+	def __init__(self, AgentType, ID, empathy0 = 0.0, empathy1 = 0.0, strat = None):
 		self.type = AgentType
-		self.Ei = empathyIn
-		self.Eo = empathyOut
+		self.ID = ID
+		self.empathy = [empathy0, empathy1]
 		if self.strategy != None:
 			self.strategy = strat
 		else:
@@ -42,14 +47,43 @@ class Agent:
 		self.reputations = [1 for i in range(NUMAGENTS)]
 
 
+
+def updateReputations(pop, reputationUpdates):
+	for i in range(len(pop)):
+
+		for j in range(len(reputationUpdates)):
+			if reputationUpdates[j] is None:
+				reputationUpdates[j] = pop[i].reputations[j]
+
+
+		pop[i].reputations = reputationUpdates
+
+	return pop
+
 game = DonationGame(1.3, 1)
 
+ALLC = np.array([1,1])
+DISC = np.array([0,1])
+ALLD = np.array([0,0])
 
-population = [Agent(int(np.floor(i / 50.0))) for i in range(NUMAGENTS)]
+STRATEGIES = [ALLC, DISC, ALLD]
+
+
+
+population = [Agent(int(np.floor(i / 50.0)), i) for i in range(NUMAGENTS)]
 
 for i in range(NUMGENERATIONS):
-	# reputationUpdates = [[None for i in NUMAGENTS] ]
+	roundPayoffs = np.zeros(NUMAGENTS)
+
+	reputationUpdates = [[None for x in range(NUMAGENTS)] for y in range(NUMAGENTS)]
+
 	for j, agent in enumerate(population):
+
+		judgers = [l for l in range(NUMAGENTS) if l != j]
+		np.random.shuffle(judgers)
+
+		count = 0
+
 		for k, adversary in enumerate(population):
 			if j != k:
 				agentRep = adversary.reputations[j]
@@ -59,10 +93,65 @@ for i in range(NUMGENERATIONS):
 				adversaryAction = adversary.strategy[agentRep]
 
 			
+				agentAction = game.moveError(agentAction)
+				adversaryAction = game.moveError(adversaryAction)
 
-				elif
+				
+				agentPayoff, adversaryPayoff = game.play(agentAction, adversaryAction)
 
-				agentPayoff, adversaryPayoff = game.play(agent.strat, adversary.strat)
+				roundPayoffs[j] += agentPayoff
+				roundPayoffs[k] += adversaryPayoff
+
+				#judgement by judger
+
+				judgeNumber = judgers[count]
+				judge = population[judgeNumber]
+
+				if np.random.random() < judge.empathy[agent.type]:
+					newrep = NORM[agentAction, adversaryRep]
+
+				else:
+					newrep = NORM[agentAction, judge.reputations[k]]
+
+				reputationUpdates[judgeNumber][j] = newrep
+
+				count += 1
+
+
+	population = updateReputations(population, reputationUpdates)
+
+	#now for strategy updating via social contagion
+
+	individuals = np.random.choice(range(NUMAGENTS), size = 2)
+
+	ind1 = individuals[0]
+	ind2 = individuals[1]
+
+	pCopy = 1.0 / ( 1 + np.exp( - w * (roundPayoffs[ind2] - roundPayoffs[ind1])))
+
+	if np.random.random() < pCopy:
+		if population[ind1].type == population[ind2].type:
+			population[ind1].strategy = population[ind2].strategy
+		else:
+			population[ind1].strategy = np.flip(population[ind2].strategy)
+
+
+	for i in range(len(population)):
+		if np.random.random() < u:
+			np.random.shuffle(STRATEGIES)
+			newstrat = STRATEGIES[0]
+
+	
+
+
+
+
+
+
+
+
+
+
 
 
 
